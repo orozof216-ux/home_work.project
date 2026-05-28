6ff420dc0406f4502facaa87ea8fff565d2f98f6
orozof216-ux <orozof216@gmail.com>
2026-05-28 13:43:02 +0600

    first commit

--- product/urls.py
+++ product/urls.py
@@ -2,12 +2,15 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('categories/', CategoryListView.as_view()),
-    path('categories/<int:id>/', CategoryDetailView.as_view()),
+    # Categories
+    path('categories/', CategoryListAPIView.as_view()),
+    path('categories/<int:pk>/', CategoryDetailAPIView.as_view()),
 
-    path('products/', ProductListView.as_view()),
-    path('products/<int:id>/', ProductDetailView.as_view()),
+    # Products
+    path('products/', ProductListAPIView.as_view()),
+    path('products/<int:pk>/', ProductDetailAPIView.as_view()),
 
-    path('reviews/', ReviewListView.as_view()),
-    path('reviews/<int:id>/', ReviewDetailView.as_view()),
+    # Reviews
+    path('reviews/', ReviewListAPIView.as_view()),
+    path('reviews/<int:pk>/', ReviewDetailAPIView.as_view()),
 ]
--- product/views.py
+++ product/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics                                                                                                                     
+from rest_framework import generics
 from .models import Category, Product, Review
 from .serializers import (
     CategorySerializer,
@@ -7,34 +7,32 @@ from .serializers import (
 )
 
 
-class CategoryListView(generics.ListAPIView):
+class CategoryListAPIView(generics.ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 
-class CategoryDetailView(generics.RetrieveAPIView):
+class CategoryDetailAPIView(generics.RetrieveAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    lookup_field = 'id'
 
 
-class ProductListView(generics.ListAPIView):
+# Product
+class ProductListAPIView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
-class ProductDetailView(generics.RetrieveAPIView):
+class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    lookup_field = 'id'
 
 
-class ReviewListView(generics.ListAPIView):
+class ReviewListAPIView(generics.ListAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
 
-class ReviewDetailView(generics.RetrieveAPIView):
+class ReviewDetailAPIView(generics.RetrieveAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
-    lookup_field = 'id'
