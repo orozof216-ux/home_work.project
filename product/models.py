d48de938ff4cd269e4cecf2e6583800720930845
orozof216-ux <orozof216@gmail.com>
2026-06-01 12:33:52 +0600

    shop api homework

--- product/models.py
+++ product/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Category(models.Model):
@@ -12,7 +13,10 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return self.title
@@ -20,7 +24,14 @@ class Product(models.Model):
 
 class Review(models.Model):
     text = models.TextField()
-    stars = models.IntegerField()
+
+    stars = models.IntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(5)
+        ]
+    )
+
     product = models.ForeignKey(
         Product,
         on_delete=models.CASCADE,
